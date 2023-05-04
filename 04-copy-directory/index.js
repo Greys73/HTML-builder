91ca12f0e072c92ae8c3be886d8c6c403bb4d0a4
Vladimir Myazin <v.myazin@gmail.com>
2023-05-04 17:16:36 +0400

    feat: task 04-copy-directory done Thu, May  4, 2023  5:16:36 PM

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+async function copyFile(fileName) {  
+  try {
+    await fs.promises.mkdir(targetDir, { recursive: true });
+    await fs.promises.copyFile(path.join(sourceDir, fileName),path.join(targetDir, fileName));
+  } catch (err) {
+    console.error(err.message);
+  }  
+}
+
+async function copyDir(folder) {
+  try {
+    const files = await fs.promises.readdir(folder,{withFileTypes: true});
+    files.forEach(file => {
+      copyFile(file.name);
+    });
+    console.log(`SUCCESS: all files was copied from ${sourceDir} to ${targetDir}`);
+  } catch (error) {
+    console.log('Error: ' + error);
+  }
+}
+
+copyDir(sourceDir);
