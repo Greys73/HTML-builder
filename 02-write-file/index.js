a69030f4db18af2906202758b584c3f301232d34
Vladimir Myazin <v.myazin@gmail.com>
2023-05-04 10:11:21 +0400

    feat: task 02-write-file done Thu, May  4, 2023 10:11:21 AM

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,33 +1,26 @@
-/*const process = require ('process');
+const process = require ('process');
 const readline = require ('readline');
 const fs = require('fs');
 const path = require('path');
 
-const fileName = 'text.txt';
+const fileName = 'output.txt';
 const filePath = path.join(__dirname, fileName);
 
-const output = fs.createWriteStream(filePath);
-const rl = readline.createInterface(process.stdin, process.stdout);
+const reader = readline.createInterface(process.stdin, process.stdout);
+const writer = fs.createWriteStream(filePath);
 
-// Начинаем чтение из stdin, чтобы процесс не завершился.
-// process.stdin.resume();
+console.log('Write some text:');
+reader.on('line',(data) => {
+  if(data.toString().trim() === 'exit'){
+    process.exit();
+  }
+  writer.write(`${data}\n`);
+});
+
+process.on('exit', () => {
+  console.log(`\nSUCCESS: text was saved to ${filePath}`);
+});
 
 process.on('SIGINT', () => {
-  console.log('SUCCESS: text was saved to ' + fileName);
-  process.exit(1);
-});*/
-const readline = require ('readline');
-
-const values = ['lorem ipsum', 'dolor sit amet'];
-const rl = readline.createInterface(process.stdin);
-const showResults = () => {
-  console.log(
-    '\n',
-    values
-      .filter((val) => val.startsWith(rl.line))
-      .join(' ')
-  );
-};
-process.stdin.on('keypress', (c, k) => {
-  showResults();
+  process.exit();
 });
